679903c213a4f54e80c87538aa3d3716a5b0f0f0
Haseeb Asif <36411814+HaseebAsif@users.noreply.github.com>
2022-06-13 15:25:31 +0500

    Search Functionality Added

--- index.js
+++ index.js
@@ -23,3 +23,21 @@
     });
   }); // end DOM ready
 })(jQuery); // end jQuery
+
+function SearchFunctionality() {
+  var input, filter, ul, li, a, i, txtValue;
+  input = document.getElementById("myInput");
+  filter = input.value.toUpperCase();
+  ul = document.getElementById("myUL");
+  li = ul.getElementsByTagName("tr");
+
+  for (i = 0; i < li.length; i++) {
+    a = li[i].getElementsByTagName("td")[0];
+    txtValue = a.textContent || a.innerText;
+    if (txtValue.toUpperCase().indexOf(filter) > -1) {
+      li[i].style.display = "";
+    } else {
+      li[i].style.display = "none";
+    }
+  }
+}
